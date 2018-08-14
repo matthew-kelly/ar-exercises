872e1467ddf063d4870de260be02cb4b9e9dfa9b
Matt Kelly <bmatthewkelly@gmail.com>
2018-08-14 19:19:09 +0000

    complete ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+# "employees" ("id" bigserial primary key, "store_id" bigint, "first_name" character varying, "last_name" character varying, "hourly_rate" integer, "created_at" timestamp NOT NULL, "updated_at" timestamp NOT NULL)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Matt", last_name: "Kelly", hourly_rate: 50)
+@store1.employees.create(first_name: "Mark", last_name: "Marcus", hourly_rate: 15)
+@store1.employees.create(first_name: "Luke", last_name: "Lucas", hourly_rate: 12)
+
+@store2.employees.create(first_name: "John", last_name: "Johnson", hourly_rate: 30)
+@store2.employees.create(first_name: "Peter", last_name: "Peterson", hourly_rate: 25)
+@store2.employees.create(first_name: "Paul", last_name: "Paulson", hourly_rate: 24)
+@store2.employees.create(first_name: "Judas", last_name: "Iscariot", hourly_rate: 14)
